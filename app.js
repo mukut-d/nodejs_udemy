127e191a964e88c1cb1abb801d35bdfb186c3172
mukut-d <mukut.m.das2000@gmail.com>
2025-06-08 01:11:34 +0530

    completed assignment 1

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const http = require("http");
 
-function fnListener(req, res) {}
-
-http.createServer(fnListener);
-
-// Using Annomymous Function
-http.createServer(function (req, res) {});
-
-// arrow functon
-
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if (url === "/") {
+    // ...
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Assignment 1</title></head>");
+    res.write(
+      `<body><form action="/create-user" method="post"><input type="text" name="username"/><button type="submit">Send</button></form></body>`
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/users") {
+    // ...
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Assignment 1</title></head>");
+    res.write(
+      "<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  // ... Send a HTML Response with some "Page not found text"
+  if (url === "/create-user") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody.split("=")[1]); //username="whatever"
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
+  }
 });
 
 server.listen(3000);
