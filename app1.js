3296d6ecddf76e2a8d21aef0a61a3e92207743f7
mukut-d <mukut.m.das2000@gmail.com>
2025-06-16 20:26:48 +0530

    made an express route

--- app1.js
+++ app1.js
@@ -1,27 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  next(); // This allows the request to continue to the next middleware in line
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1> Hello from Express </h1>");
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(3000);
