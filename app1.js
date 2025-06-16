648cffc237f9c655c14086e849756dd0608b1ac3
mukut-d <mukut.m.das2000@gmail.com>
2025-06-16 16:04:34 +0530

    made two routes

--- app1.js
+++ app1.js
@@ -7,7 +7,12 @@ app.use((req, res, next) => {
   next(); // This allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In add product ");
+  res.send("<h1>The 'Add Product' Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware");
   res.send("<h1> Hello from Express </h1>");
 });
