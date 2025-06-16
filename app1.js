ff08d9b1c4c41a16e0445127dab2d3b527abd372
mukut-d <mukut.m.das2000@gmail.com>
2025-06-16 19:47:51 +0530

    worked with res.redirect()

--- app1.js
+++ app1.js
@@ -1,19 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-  console.log("In the middleware");
   next(); // This allows the request to continue to the next middleware in line
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In add product ");
-  res.send("<h1>The 'Add Product' Page</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
   res.send("<h1> Hello from Express </h1>");
 });
 
