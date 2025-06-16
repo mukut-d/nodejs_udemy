761e0adaa816ec5aee3ebea31d02f2d60d0bdf05
mukut-d <mukut.m.das2000@gmail.com>
2025-06-16 20:53:35 +0530

    made a route for 404 page

--- app1.js
+++ app1.js
@@ -11,4 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRoutes);
 app.use(adminRoutes);
 
+// catch all routes
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000);
